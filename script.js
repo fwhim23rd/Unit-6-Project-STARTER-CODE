08aeaeb12e08969cb5657148315fdea9e8506379
fwhim23rd <139658111+fwhim23rd@users.noreply.github.com>
2023-07-15 15:37:19 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+// Declare your genre arrays here
+
+// Make sure to declare your HTML elements as variables! 
+
+// Submit Button 
+let submitButton = document.querySelector("button"); 
+submitButton.onclick = function() {
+}; 
+let songList =["calling", "choose up" , "sky priority"];
+console.log(songList);
+
